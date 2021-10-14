193be949357c1fad12bbb5dc2571b6a912746537
mortonjt <jamietmorton@gmail.com>
2021-10-14 10:10:09 -0600

    initial commit

---
+++ scripts/extract_seqs.py
@@ -0,0 +1,19 @@
+import argparse
+import biom
+
+
+def main(args):
+    table = biom.load_table(args.table)
+    ids = table.ids(axis='observation')
+    with open('seqs.fa', 'w') as f:
+       f.write('\n'.join(list(map(lambda x: f'>{x}\n{x}', ids))))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(add_help=True)
+    parser.add_argument('--table', type=str, required=True,
+                        help='Path to biom table.')
+    parser.add_argument('--output-fasta', type=str, required=True,
+                        help='Path to output fasta file.')
+    args = parser.parse_args()
+    main(args
---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import find_packages, setup
+from glob import glob
+
+
+classes = """
+    Development Status :: 4 - Beta
+    License :: OSI Approved :: BSD License
+    Topic :: Software Development :: Libraries
+    Topic :: Scientific/Engineering
+    Topic :: Scientific/Engineering :: Bio-Informatics
+    Programming Language :: Python :: 3
+    Programming Language :: Python :: 3 :: Only
+    Programming Language :: Python :: 3.4
+    Programming Language :: Python :: 3.5
+    Programming Language :: Python :: 3.6
+    Programming Language :: Python :: 3.7
+    Operating System :: Unix
+    Operating System :: POSIX
+    Operating System :: MacOS :: MacOS X
+"""
+classifiers = [s.strip() for s in classes.split('\n') if s]
+
+description = ('Biom utility scripts.')
+
+
+setup(name='biom-util',
+      version='0.0.2',
+      license='BSD-3-Clause',
+      description=description,
+      author_email="jamietmorton@gmail.com",
+      maintainer_email="jamietmorton@gmail.com",
+      packages=find_packages(),
+      install_requires=[
+          'numpy',
+          'scipy',
+          'biom-format',
+          'pandas>=1.0.0',
+          # if utilizing the qiime2 API you also need a mininum of
+          # https://github.com/qiime2/qiime2.git
+          # https://github.com/qiime2/q2-sample-classifier.git
+          # https://github.com/qiime2/q2-types.git
+      ],
+      classifiers=classifiers,
+      package_data={},
+      scripts=glob('scripts/*'))
