f200e2cc40ac1aceebc2bfa8697abf938f212473
mortonjt <jamietmorton@gmail.com>
2021-10-14 18:44:54 -0400

    updating collapse script

--- scripts/collapse_alignments.py
+++ scripts/collapse_alignments.py
@@ -2,16 +2,21 @@
 import argparse
 import biom
 from biom.util import biom_open
+import numpy as np
 import pandas as pd
+from scipy.sparse import coo_matrix
+import scipy as sp
 
 
 def main(args):
     table = biom.load_table(args.table)
     aligns = pd.read_table(args.align, header=None)
-    seq_to_genome = aligns[[0, 2]].set_index(0)
-    bin_f = lambda x: seq_to_genome.loc[x]
-    t = table.collapse(bin_f, norm=False, min_group_size=1,
-                       axis='observation').sort(axis='observation')
+    seq_to_genome = aligns[[0, 2, 4]].groupby(by=[0]).min()[2]
+    ids = list(set(seq_to_genome.index) & set(table.ids(axis='observation')))
+    t = table.filter(ids, axis='observation')
+    bin_f = lambda i, x: seq_to_genome.loc[i]
+    t = t.collapse(bin_f, norm=False, min_group_size=1, axis='observation')
+    #t = groupby_sum(t, seq_to_genome)
     with biom_open(args.output_table, 'w') as f:
         t.to_hdf5(f, 'collapsed')
 
